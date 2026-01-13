1795253eee75666dd8aa6ebc6568942bc453fda8
Fiqiri <fiqiripoleshi@gmail.com>
2026-01-13 17:16:27 +0100

    Fix CSRF for Railway domain

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
+
+
